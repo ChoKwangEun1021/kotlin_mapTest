b9bcedbb5a9768401f739f4899f0ca696e578fcd
Cho KwangEun <rhkddms1021@gmail.com>
2023-04-25 17:46:41 +0900

    [update] 원하는 위치 클릭시 커스텀 마커 추가, 메인화면 슬라이딩드로우 수정

--- app/src/main/java/com/jaewook/android_kakaomap/MainActivity.kt
+++ app/src/main/java/com/jaewook/android_kakaomap/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.jaewook.android_kakaomap
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jaewook.android_kakaomap.data.KakaoAPI
 import com.jaewook.android_kakaomap.data.ResultSearchKeyword
@@ -20,10 +28,10 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MapView.MapViewEventListener, View.OnClickListener {
     companion object {
         const val BASE_URL = "https://dapi.kakao.com/"
-        const val API_KEY = "KakaoAK 내API키"  // REST API 키
+        const val API_KEY = "KakaoAK 본인API키"  // REST API 키
     }
 
     lateinit var binding: ActivityMainBinding
@@ -33,10 +41,14 @@ class MainActivity : AppCompatActivity() {
     private val listAdapter = ListAdapter(listItems)    // 리사이클러 뷰 어댑터
     private var pageNumber = 1      // 검색 페이지 번호
     private var keyword = ""        // 검색 키워드
+    private val ACCESS_FINE_LOCATION = 777 // Request Code
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        val slidePanel = binding.mainFrame                      // SlidingUpPanel
+//        slidePanel.addPanelSlideListener(PanelEventListener())  // 이벤트 리스너 추가
 
         mapView = binding.mapView  // 카카오 지도 뷰
         mapView.setCalloutBalloonAdapter(CustomBalloonAdapter(layoutInflater))  // 커스텀 말풍선 등록
@@ -76,21 +88,83 @@ class MainActivity : AppCompatActivity() {
             searchKeyword(keyword, pageNumber)
         }
 
-        // 서울시청에 마커 추가
-        val marker = MapPOIItem()
-        marker.apply {
-            itemName = "서울시청"   // 마커 이름
-            mapPoint = MapPoint.mapPointWithGeoCoord(37.5666805, 126.9784147)   // 좌표
-            markerType = MapPOIItem.MarkerType.CustomImage          // 마커 모양 (커스텀)
-            customImageResourceId = R.drawable.pin               // 커스텀 마커 이미지
-            selectedMarkerType = MapPOIItem.MarkerType.CustomImage  // 클릭 시 마커 모양 (커스텀)
-            customSelectedImageResourceId = R.drawable.pin       // 클릭 시 커스텀 마커 이미지
-            isCustomImageAutoscale = false      // 커스텀 마커 이미지 크기 자동 조정
-            setCustomImageAnchor(0.5f, 1.0f)    // 마커 이미지 기준점
+
+        binding.btnAddMaker.setOnClickListener(this)
+        binding.btnTarget.setOnClickListener(this)
+    }
+
+    // 위치 권한 확인
+    private fun permissionCheck() {
+        val preference = getPreferences(MODE_PRIVATE)
+        val isFirstCheck = preference.getBoolean("isFirstPermissionCheck", true)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 없는 상태
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // 권한 거절 (다시 한 번 물어봄)
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage("현재 위치를 확인하시려면 위치 권한을 허용해주세요.")
+                builder.setPositiveButton("확인") { dialog, which ->
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
+                }
+                builder.setNegativeButton("취소") { dialog, which ->
+
+                }
+                builder.show()
+            } else {
+                if (isFirstCheck) {
+                    // 최초 권한 요청
+                    preference.edit().putBoolean("isFirstPermissionCheck", false).apply()
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
+                } else {
+                    // 다시 묻지 않음 클릭 (앱 정보 화면으로 이동)
+                    val builder = AlertDialog.Builder(this)
+                    builder.setMessage("현재 위치를 확인하시려면 설정에서 위치 권한을 허용해주세요.")
+                    builder.setPositiveButton("설정으로 이동") { dialog, which ->
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:$packageName"))
+                        startActivity(intent)
+                    }
+                    builder.setNegativeButton("취소") { dialog, which ->
+
+                    }
+                    builder.show()
+                }
+            }
+        } else {
+            // 권한이 있는 상태
+            startTracking()
         }
-        mapView.addPOIItem(marker)
+    }
 
+    // 권한 요청 후 행동
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == ACCESS_FINE_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한 요청 후 승인됨 (추적 시작)
+                Toast.makeText(this, "위치 권한이 승인되었습니다", Toast.LENGTH_SHORT).show()
+                startTracking()
+            } else {
+                // 권한 요청 후 거절됨 (다시 요청 or 토스트)
+                Toast.makeText(this, "위치 권한이 거절되었습니다", Toast.LENGTH_SHORT).show()
+                permissionCheck()
+            }
+        }
+    }
 
+    // GPS가 켜져있는지 확인
+    private fun checkLocationService(): Boolean {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    // 위치추적 시작
+    private fun startTracking() {
+        binding.mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+    }
+
+    // 위치추적 중지
+    private fun stopTracking() {
+        binding.mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
     }
 
     // 키워드 검색 함수
@@ -190,4 +264,54 @@ class MainActivity : AppCompatActivity() {
             // 마커의 속성 중 isDraggable = true 일 때 마커를 이동시켰을 경우
         }
     }
-}
+
+    override fun onMapViewInitialized(p0: MapView?) {}
+
+    override fun onMapViewCenterPointMoved(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onMapViewZoomLevelChanged(p0: MapView?, p1: Int) {}
+
+    override fun onMapViewSingleTapped(p0: MapView?, p1: MapPoint?) {
+        // 마커 생성
+        val marker = MapPOIItem()
+        marker.itemName = "마커 이름"
+        marker.tag = 0
+        marker.mapPoint = MapPoint.mapPointWithGeoCoord(p1?.mapPointGeoCoord!!.latitude, p1?.mapPointGeoCoord!!.longitude)
+        marker.markerType = MapPOIItem.MarkerType.CustomImage          // 마커 모양 (커스텀)
+        marker.customImageResourceId = R.drawable.pin               // 커스텀 마커 이미지
+        marker.selectedMarkerType = MapPOIItem.MarkerType.CustomImage  // 클릭 시 마커 모양 (커스텀)
+        marker.customSelectedImageResourceId = R.drawable.blue_pin       // 클릭 시 커스텀 마커 이미지
+        marker.isCustomImageAutoscale = false      // 커스텀 마커 이미지 크기 자동 조정
+        marker.setCustomImageAnchor(0.5f, 1.0f)    // 마커 이미지 기준점
+        // 지도에 마커 추가
+        mapView.addPOIItem(marker)
+    }
+
+    override fun onMapViewDoubleTapped(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onMapViewLongPressed(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onMapViewDragStarted(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onMapViewDragEnded(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onMapViewMoveFinished(p0: MapView?, p1: MapPoint?) {}
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_addMaker -> {
+                mapView.setMapViewEventListener(this)
+            }
+            R.id.btn_target -> {
+                if (checkLocationService()) {
+                    // GPS가 켜져있을 경우
+                    permissionCheck()
+                } else {
+                    // GPS가 꺼져있을 경우
+                    Toast.makeText(this, "GPS를 켜주세요", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+}// end of class
